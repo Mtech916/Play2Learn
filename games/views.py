1344e7d4fddcc70842ee647b07b0ad02676b34eb
Mtech916 <shredtechfit@gmail.com>
2023-10-02 13:59:07 -0700

    Setup Initial Website Structure

---
+++ games/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class MathFactsView(TemplateView):
+    template_name = "games/math-facts.html"
+
+
+class AnagramHuntView(TemplateView):
+    template_name = "games/anagram-hunt.html"

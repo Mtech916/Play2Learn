e45cb6d4ba5960366993d413fa19dc0e752aa69b
Mtech916 <shredtechfit@gmail.com>
2023-10-09 11:00:51 -0700

    Created game score admin class

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from games.models import GameScore
+
+
+@admin.register(GameScore)
+class GameScoreAdmin(admin.ModelAdmin):
+    model = GameScore
+    list_display = ["user_name", "game", "score", "created"]
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return "created"
+
+        return ()

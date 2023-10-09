28de3e9d983522f4d0435b6ddcde7bc1ba520528
Mtech916 <shredtechfit@gmail.com>
2023-10-09 14:33:15 -0700

    Setting up SendGrid and User Authentication

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -39,13 +39,25 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     # Local Apps
     "common.apps.CommonConfig",
     "games.apps.GamesConfig",
     "pages.apps.PagesConfig",
     "users.apps.UsersConfig",
 ]
+
+SITE_ID = 1
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -55,6 +67,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Third-party middleware
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "play2learn.urls"
@@ -88,6 +102,21 @@ DATABASES = {
     }
 }
 
+# Email
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+DEFAULT_FROM_EMAIL = "agenthiggins@gmail.com"
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, even w/o `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth`-specific auth methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -109,6 +138,21 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # AUTHENTICATION SETTINGS
 AUTH_USER_MODEL = "users.CustomUser"
+
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = "users.CustomUser"
+LOGIN_URL = "account_login"
+LOGIN_REDIRECT_URL = "pages:homepage"
+
+# django-allauth settings
+ACCOUNT_AUTHENTICATION_METHOD = "email"  # Default: 'username'
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1  # Default: 3
+ACCOUNT_EMAIL_REQUIRED = True  # Default: False
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"  # Default: 'optional'
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 5  # Default: 5
+ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 300  # Default 300
+ACCOUNT_LOGOUT_REDIRECT_URL = "account_login"  # Default: '/'
+ACCOUNT_USERNAME_REQUIRED = True  # Default: True
 
 
 # Internationalization
--- play2learn/urls.py
+++ play2learn/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #  Admin
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
-    path("", include("pages.urls")),
+    # User Management
+    path("account/", include("allauth.urls")),
+    # Local Apps
     path("games/", include("games.urls")),
+    path("", include("pages.urls")),
 ]

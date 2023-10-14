069176d8da3541f48bfa96816aca67956ed199d9
Mtech916 <shredtechfit@gmail.com>
2023-10-14 15:19:49 -0700

    Added avatar + profile pic

--- users/forms.py
+++ users/forms.py
@@ -22,7 +22,7 @@ class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
-        fields = ("email", "username", "first_name", "last_name", "dob")
+        fields = ("email", "username", "first_name", "last_name", "dob", "avatar")
         widgets = {
             "dob": forms.SelectDateWidget(
                 attrs={"style": "width: 25%; display: inline-flex; margin: 0 1%"},

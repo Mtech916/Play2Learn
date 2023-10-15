d323f27a9062d6b106faff3fc3d9cf472c582090
Mtech916 <shredtechfit@gmail.com>
2023-10-15 13:04:12 -0700

    add formView w/contct form and auto email

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from pages.views import ContactUsPageView, HomePageView, AboutPageView
+from pages.views import (
+    ContactUsPageView,
+    ContactUsThanksView,
+    HomePageView,
+    AboutPageView,
+)
 
 app_name = "pages"
 urlpatterns = [
     path("", HomePageView.as_view(), name="homepage"),
     path("about/", AboutPageView.as_view(), name="about"),
-    path("contact/", ContactUsPageView.as_view(), name="contact-us"),
+    path("contact-us/", ContactUsPageView.as_view(), name="contact-us"),
+    path("contact-us/thanks/", ContactUsThanksView.as_view(), name="thanks"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,36 @@
-from django.views.generic import TemplateView
+import html
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
+
+from common.utils.email import send_email
+from .forms import ContactUsForm
 
 
-class ContactUsPageView(TemplateView):
-    template_name = "pages/contact.html"
+class ContactUsPageView(FormView):
+    template_name = "pages/contact_us.html"
+    form_class = ContactUsForm
+    success_url = reverse_lazy("pages:thanks")
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        to = "agenthiggins@gmail.com"
+        subject = "Your message has been received"
+        content = f"""<p>Attention Play2Learn Admin!</p>
+                <p>Website visitor contact message received:</p>
+                <ol>"""
+        for key, value in data.items():
+            label = key.replace("_", " ").title()
+            entry = html.escape(str(value), quote=False)
+            content += f"<li>{label}: {entry}</li>"
+
+        content += "</ol>"
+
+        send_email(to, subject, content)
+        return super().form_valid(form)
+
+
+class ContactUsThanksView(TemplateView):
+    template_name = "pages/thanks.html"
 
 
 class HomePageView(TemplateView):

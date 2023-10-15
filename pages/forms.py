9ad00a85e89870a67199727497bd807fc15b1a73
Mtech916 <shredtechfit@gmail.com>
2023-10-15 13:20:14 -0700

    add new contct form fields

--- pages/forms.py
+++ pages/forms.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
+    EMAIL_TOPIC = (
+        (None, "--Please Choose--"),
+        ("general", "General Question"),
+        ("technical", "Technical Error"),
+        ("request", "Feature Request"),
+        ("other", "Not Listed"),
+    )
     first_name = forms.CharField()
     last_name = forms.CharField()
+    subject = forms.ChoiceField(choices=EMAIL_TOPIC)
     email = forms.EmailField()
+    message = forms.CharField()
